9065c68802cbcdbbca9f59620f4ba444c854791a
noha20009 <machkourinouhayla@gmail.com>
2026-04-27 12:32:47 +0100

    update dtoPatient

--- src/main/java/org/example/systemegestionmedicale/DTO/MedecinDTO.java
+++ src/main/java/org/example/systemegestionmedicale/DTO/MedecinDTO.java
@@ -1,4 +1,24 @@
 package org.example.systemegestionmedicale.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class MedecinDTO {
+    private Long id;
+
+    @NotBlank(message = "Le nom est obligatoire")
+    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "La spécialité est obligatoire")
+    @Size(max = 100, message = "La spécialité ne doit pas dépasser 100 caractères")
+    private String specialite;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Email invalide")
+    private String email;
+    @Size(min = 10,max = 10,message ="Numéro de téléphone invalide")
+    private String telephone;
 }
+
