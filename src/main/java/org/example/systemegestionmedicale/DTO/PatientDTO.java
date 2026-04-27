92cee3eee9b79ab1ee5f1d24a145df2406c12f44
noha20009 <machkourinouhayla@gmail.com>
2026-04-27 12:01:40 +0100

    validation dtoPatient

--- src/main/java/org/example/systemegestionmedicale/DTO/PatientDTO.java
+++ src/main/java/org/example/systemegestionmedicale/DTO/PatientDTO.java
@@ -1,4 +1,31 @@
 package org.example.systemegestionmedicale.DTO;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class PatientDTO {
+    private Long id;
+    @NotBlank(message = "Le nom est obligatoire")
+    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
+    private String nom;
+
+    @NotBlank(message = "Le prénom est obligatoire")
+    @Size(max = 100, message = "Le prénom ne doit pas dépasser 100 caractères")
+    private String prenom;
+
+    @NotBlank(message = "L'email est obligatoire")
+    @Email(message = "Email invalide")
+    @Size(max = 150)
+    private String email;
+
+    @Size(min =10,max = 10,message = "Numéro de téléphone invalide")
+    private String telephone;
+    @Past(message = "La date de naissance doit être dans le passé")
+    private LocalDate dateNaissance;
 }
