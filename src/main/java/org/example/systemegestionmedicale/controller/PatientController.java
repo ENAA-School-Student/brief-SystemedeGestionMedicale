587513124302e31ee91d4dccfa13e81926f158f8
noha20009 <machkourinouhayla@gmail.com>
2026-04-28 12:57:43 +0100

    ajout controller patient

--- src/main/java/org/example/systemegestionmedicale/controller/PatientController.java
+++ src/main/java/org/example/systemegestionmedicale/controller/PatientController.java
@@ -1,7 +1,38 @@
 package org.example.systemegestionmedicale.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.systemegestionmedicale.DTO.PatientDTO;
+import org.example.systemegestionmedicale.service.Patientservie;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/patients")
 public class PatientController {
+    private final Patientservie patientservie;
+
+    @PostMapping
+    public PatientDTO createPatient(@Valid @RequestBody PatientDTO dto){
+        return patientservie.ajouterPatient(dto);
+    }
+
+    @GetMapping
+    public List<PatientDTO> getAllPatients(){
+        return patientservie.getAllPatients();
+    }
+    @GetMapping
+    public PatientDTO getById(@PathVariable Long id){
+        return patientservie.getPatientById(id);
+    }
+    @PutMapping
+    public PatientDTO updatePatient(@PathVariable Long id,@Valid @RequestBody PatientDTO dto){
+        return patientservie.modifierPatient(id,dto);
+    }
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable Long id){
+         patientservie.delete(id);
+    }
 }
