95a8c83ac827914e4b576d4e47983a729ba38eff
noha20009 <machkourinouhayla@gmail.com>
2026-04-29 17:28:37 +0100

    test creerdossierMedical/ajouterDiagnostic

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="enaa"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/test/java/org/example/systemegestionmedicale/service/DossierMedicalServiceTest.java
@@ -0,0 +1,16 @@
+package org.example.systemegestionmedicale.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DossierMedicalServiceTest {
+
+    @Test
+    void creerDossier() {
+    }
+
+    @Test
+    void ajouterDiagnostic() {
+    }
+}
