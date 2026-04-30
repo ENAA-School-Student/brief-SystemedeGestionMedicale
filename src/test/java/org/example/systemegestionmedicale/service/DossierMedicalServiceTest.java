c9a970a93fd3b1d6aca5ce15819f4943b6b63827
noha20009 <machkourinouhayla@gmail.com>
2026-04-30 09:37:17 +0100

    update

--- src/test/java/org/example/systemegestionmedicale/service/DossierMedicalServiceTest.java
+++ src/test/java/org/example/systemegestionmedicale/service/DossierMedicalServiceTest.java
@@ -1,16 +1,70 @@
 package org.example.systemegestionmedicale.service;
 
+import jakarta.transaction.Transactional;
+import org.example.systemegestionmedicale.DTO.DossierMedicalDTO;
+import org.example.systemegestionmedicale.DTO.PatientDTO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
 class DossierMedicalServiceTest {
+    @Autowired
+    DossierMedicalService dossierMedicalService;
+    @Autowired
+    PatientService patientService;
 
     @Test
     void creerDossier() {
+        PatientDTO patient = new PatientDTO();
+        patient.setNom("Sara");
+        PatientDTO savedPatient = patientService.ajouterPatient(patient);
+
+
+        DossierMedicalDTO dossierDTO = new DossierMedicalDTO();
+        dossierDTO.setPatientId(savedPatient.getId());
+        DossierMedicalDTO savedDossier = dossierMedicalService.creerDossier(dossierDTO);
+
+
+        DossierMedicalDTO updated = dossierMedicalService
+                .ajouterDiagnostic(savedDossier.getId(), "Grippe");
+
+
+        assertNotNull(updated);
+        assertEquals("Grippe", updated.getDiagnostic());
     }
 
     @Test
-    void ajouterDiagnostic() {
+    void testAjouterDiagnostic_NotFound() {
+        assertThrows(RuntimeException.class, () -> {
+            dossierMedicalService.ajouterDiagnostic(999L, "Test");
+        });
     }
-}
+    @Test
+    void testAjouterDiagnostic() {
+
+        PatientDTO patient = new PatientDTO();
+        patient.setNom("Sara");
+        PatientDTO savedPatient = patientService.ajouterPatient(patient);
+
+
+        DossierMedicalDTO dossierDTO = new DossierMedicalDTO();
+        dossierDTO.setPatientId(savedPatient.getId());
+        DossierMedicalDTO savedDossier = dossierMedicalService.creerDossier(dossierDTO);
+
+
+        DossierMedicalDTO updated = dossierMedicalService
+                .ajouterDiagnostic(savedDossier.getId(), "Grippe");
+
+
+        assertNotNull(updated);
+        assertEquals("Grippe", updated.getDiagnostic());
+    }
+
+    }
+
+
